19aebe6a6d446a090d9c1fea44d3f95b4cdd76fc
Popadynets-crypto <popadunets@gmail.com>
2025-06-25 00:27:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ BOT_TOKEN = "7282256884:AAE7Gy77brpbxZqQJc6Bq_sOtVu28u_329E"
 
 #Binance API (не обов'язково для простого аналізу, але потрібен для торгівлі)
 
-BINANCE_API_KEY = "встав_сюди_API_KEY" BINANCE_API_SECRET = "встав_сюди_API_SECRET"
+BINANCE_API_KEY = "tvSxvYElHJxUH5Z3S89wPriecY79PVpPjhO6xYRWt5RB7YIN16TevfESYOvyiC4s" 
+BINANCE_API_SECRET = "S7vO1jJuX27NY0jWqn1TUWKR62dEzQ5hY1nlBPFeSDPac4chDfUIejGfrFO36kNE"
 
 #Монети для аналізу
 
