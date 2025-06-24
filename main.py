b0dc10e890d3691b18883bf623d81c8d24108296
Popadynets-crypto <popadunets@gmail.com>
2025-06-25 00:06:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 import config
