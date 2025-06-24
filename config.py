a4280c8c2207496bcb749b38c04b3422d6ebe54e
Popadynets-crypto <popadunets@gmail.com>
2025-06-25 01:29:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,7 @@
-#Telegram
-
 BOT_TOKEN = "7282256884:AAE7Gy77brpbxZqQJc6Bq_sOtVu28u_329E"
-
-#Binance API (не обов'язково для простого аналізу, але потрібен для торгівлі)
-
 BINANCE_API_KEY = "tvSxvYElHJxUH5Z3S89wPriecY79PVpPjhO6xYRWt5RB7YIN16TevfESYOvyiC4s" 
 BINANCE_API_SECRET = "S7vO1jJuX27NY0jWqn1TUWKR62dEzQ5hY1nlBPFeSDPac4chDfUIejGfrFO36kNE"
 
-#Монети для аналізу
-
 symbols = ["BTC", "ETH", "PEPE", "DOGE", "SOL", "ADA"]
-
-#Інтервал аналізу в хвилинах (для автозапуску)
-
 ANALYSIS_INTERVAL_MINUTES = 15
-ALERT_CHAT_ID = 914674704  # ← твій справжній ID сюди
+ALERT_CHAT_ID = 914674704
