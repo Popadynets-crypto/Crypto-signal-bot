30bea63b588450d4961c09ce0f8a45f14a2c3de6
Popadynets-crypto <popadunets@gmail.com>
2025-06-25 00:19:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 config.py
 
-Telegram
+#Telegram
 
 BOT_TOKEN = "7282256884:AAE7Gy77brpbxZqQJc6Bq_sOtVu28u_329E"
 
-Binance API (не обов'язково для простого аналізу, але потрібен для торгівлі)
+#Binance API (не обов'язково для простого аналізу, але потрібен для торгівлі)
 
 BINANCE_API_KEY = "встав_сюди_API_KEY" BINANCE_API_SECRET = "встав_сюди_API_SECRET"
 
-Монети для аналізу
+#Монети для аналізу
 
 symbols = ["BTC", "ETH", "PEPE", "DOGE", "SOL", "ADA"]
 
-Інтервал аналізу в хвилинах (для автозапуску)
+#Інтервал аналізу в хвилинах (для автозапуску)
 
 ANALYSIS_INTERVAL_MINUTES = 15
-
